7b9bfdd8ecd27af6df0b11ef2aae10296891a138
ahnchanghee4798 <119610308+ahnchanghee4798@users.noreply.github.com>
2022-12-08 11:03:01 +0900

    commit

--- customer/src/main/java/exmall/domain/OrderRejected.java
+++ customer/src/main/java/exmall/domain/OrderRejected.java
@@ -1,21 +1,19 @@
 package exmall.domain;
 
-import exmall.domain.*;
 import exmall.infra.AbstractEvent;
-import lombok.*;
+import lombok.Data;
 import java.util.*;
+
+
 @Data
-@ToString
 public class OrderRejected extends AbstractEvent {
 
     private Long id;
     private String status;
     private String foodId;
     private String orderId;
-    private Object options;
+    private List<String> options;
     private Integer qty;
     private String storeId;
     private String customerId;
 }
-
-
